b5cde585659f8b8150c412202ba7c4e9d4a26816
Vaidas Kascėnas <vaidas@kascenas.lt>
2020-08-09 17:14:31 +0300

    bash fix

--- debootscript.sh
+++ debootscript.sh
@@ -298,9 +298,8 @@ chroot_actions() {
   apt-get autoremove
   apt-get clean
 }
-export -f chroot_actions
 export target_hostname use_lvm partition_type distro target_user target_password ssh_public_key
-chroot /target /bin/bash -O nullglob -O extglob -ec chroot_actions
+chroot /target /bin/bash -O nullglob -O extglob -ec "$(declare -f chroot_actions) && chroot_actions"
 
 ###########
 # Cleanup #
